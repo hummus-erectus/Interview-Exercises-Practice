f41630bcc152bb000c0ebf6c9739510da428eda9
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-02-17 23:49:14 +0900

    Create getSaleItemsFunction.js

---
+++ getSaleItemsFunction.js
@@ -0,0 +1,46 @@
+import products from "./data.js";
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+// My solution
+function getSaleItems(data){
+    return data.filter(product => product.type === "sweet").map(product => (
+        {item: product.item, price:product.price}
+    ))
+};
+
+// Suggested solution
+// function getSaleItems(data){
+//     // filter the data by product.type -- only sweet
+//     return data
+//     .filter(product => product.type === "sweet")
+//     // loop through the data using map
+//     .map(({item, price}) => {
+//         // for every candy, return a new object with only item and price
+//         return {
+//             item,
+//             price
+//         }
+//     });
+// };
+
+console.log(getSaleItems(products))
+
+// Comments
+// Good to destructure
+
